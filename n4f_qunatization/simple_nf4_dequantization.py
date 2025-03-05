43524b679b4317e9d0fef641b93e15a756ad006d
Adam Myers <adam.myers1909@gmail.com>
2025-03-05 22:14:12 +0000

    Link added to blockwize

--- n4f_qunatization/simple_nf4_dequantization.py
+++ n4f_qunatization/simple_nf4_dequantization.py
@@ -93,3 +93,4 @@ if __name__ == "__main__":
     print(X_dequant[:5, :5])
     
     # TODO: blockwize dequnatization, signature to match fast_dequantize
+    # https://github.com/bitsandbytes-foundation/bitsandbytes/blob/86b6c37a8ad448230cedb60753f63150b603a112/bitsandbytes/functional.py#L958
